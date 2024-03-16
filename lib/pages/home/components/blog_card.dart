634ed57613198ecf495caae30f4b7af8fd15fffe
Cano XL <gcanoxl@gmail.com>
2024-03-16 23:08:26 +0800

    feat: improve blog_card's layout

--- lib/pages/home/components/blog_card.dart
+++ lib/pages/home/components/blog_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:geeblog/constants.dart';
 import 'package:geeblog/models/blog.dart';
 import 'package:get/get.dart';
@@ -18,40 +19,44 @@ class BlogCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final String time = parseTimestamp(blog.created_at);
-    return InkWell(
-      onTap: () {
-        Get.toNamed('/article?id=${blog.id}');
-      },
-      child: ConstrainedBox(
-        constraints: const BoxConstraints(minWidth: double.infinity),
-        child: Container(
-          margin: const EdgeInsets.only(bottom: defaultPadding / 2),
-          padding: const EdgeInsets.symmetric(
-              horizontal: defaultPadding / 2, vertical: defaultPadding / 2),
-          decoration: BoxDecoration(
-            borderRadius: const BorderRadius.all(Radius.circular(10)),
-            border: Border.all(color: Colors.grey.withOpacity(0.2)),
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                blog.title,
-                style: const TextStyle(
-                  fontSize: defaultPadding,
+    return Padding(
+      padding: const EdgeInsets.only(bottom: defaultPadding / 2),
+      child: InkWell(
+        onTap: () {
+          Get.toNamed('/article?id=${blog.id}');
+        },
+        child: ConstrainedBox(
+          constraints: const BoxConstraints(minWidth: double.infinity),
+          child: Container(
+            padding: const EdgeInsets.symmetric(
+              horizontal: defaultPadding / 2,
+              vertical: defaultPadding / 2,
+            ),
+            decoration: BoxDecoration(
+              borderRadius: const BorderRadius.all(Radius.circular(10)),
+              border: Border.all(color: Colors.grey.withOpacity(0.2)),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  blog.title,
+                  style: const TextStyle(
+                    fontSize: defaultPadding,
+                  ),
                 ),
-              ),
-              const SizedBox(height: defaultPadding / 2),
-              Text(
-                '${'created_at'.tr}: $time',
-                style: TextStyle(
-                  fontSize: defaultPadding * 0.6,
-                  color: Colors.black.withOpacity(defaultOpacity),
+                const SizedBox(height: defaultPadding / 2),
+                Text(
+                  '${'created_at'.tr}: $time',
+                  style: TextStyle(
+                    fontSize: defaultPadding * 0.6,
+                    color: Colors.black.withOpacity(defaultOpacity),
+                  ),
                 ),
-              ),
-              const SizedBox(height: defaultPadding / 2),
-              Text(blog.content),
-            ],
+                const SizedBox(height: defaultPadding / 2),
+                MarkdownBody(data: blog.content),
+              ],
+            ),
           ),
         ),
       ),
